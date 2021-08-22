f21a8c236a42cac48c2738f58aff8c11556ca866
Victor Iris <victormidp@gmail.com>
2021-08-22 03:53:49 -0500

    chore(deps): upgrade apollo server

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,30 +1,39 @@
 import { ApolloServer } from "apollo-server-micro";
-import {
-	makeSchema,
-} from "nexus";
+import { NextApiRequest, NextApiResponse } from "next";
+import { makeSchema } from "nexus";
 import path from "path";
 import * as types from "../../schema";
 
-export const schema = makeSchema({
-	types,
-	outputs: {
-		typegen: path.join(process.cwd(), "generated/nexus-typegen.ts"),
-		schema: path.join(process.cwd(), "generated/schema.graphql"),
-	},
-});
-
+let apolloServerHandler: ReturnType<typeof ApolloServer.prototype.createHandler>;
 export const config = {
 	api: {
 		bodyParser: false,
 	},
 };
 
-const apolloServer = new ApolloServer({
-	schema,
-	introspection: true,
-	playground: true
-});
+const getApolloServerHandler = async () => {
+	if (!apolloServerHandler) {
+		const schema = makeSchema({
+			types,
+			outputs: {
+				typegen: path.join(process.cwd(), "generated/nexus-typegen.ts"),
+				schema: path.join(process.cwd(), "generated/schema.graphql"),
+			},
+		});
 
-export default apolloServer.createHandler({
-	path: "/api/graphql",
-});
+		const server = new ApolloServer({
+			schema,
+			introspection: true,
+		})
+		await server.start()
+		apolloServerHandler = server.createHandler({
+			path: "/api/graphql",
+		});
+	}
+	return apolloServerHandler;
+};
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+	const apolloServerHandler = await getApolloServerHandler();
+	return apolloServerHandler(req, res);
+};
